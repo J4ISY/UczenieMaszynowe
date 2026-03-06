76a8756de75e4b3a9b29c062e61f174fd782e82f
J4ISY <kubamar02@gmail.com>
2026-03-06 10:59:26 +0100

    RWTabular added convert to xlsx, file in data

--- RWTabularData.py
+++ RWTabularData.py
@@ -1,8 +1,11 @@
 import pandas as pd
 
 titanic = pd.read_csv("data/titanic.csv")
+titanicXLSX = pd.read_excel("data/titanic.xlsx", sheet_name="passengers")
 
 # print(titanic) # First 5 and Last 5 rows
 # print(titanic.head(50)) # 50 Heads
 # print(titanic.dtypes) # Data Types
 # print(titanic.info()) # Technical info
+# titanic.to_excel("data/titanic.xlsx", sheet_name="passengers", index=False) # File XLSX EXCEL
+# print(titanicXLSX.head())
