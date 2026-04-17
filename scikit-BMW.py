dd6673590c7c263778ee9259bdb32ae0278f2968
J4ISY <kubamar02@gmail.com>
2026-04-17 11:24:23 +0200

    another task for machine learning at UNi

--- scikit-BMW.py
+++ scikit-BMW.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn import tree
+from sklearn.metrics import mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
 
 data = pd.read_csv('data/bmw_global_sales_2018_2025.csv') # Import D A N Y C H
 
@@ -10,15 +12,13 @@ clean_data = clean_data.dropna() #usunece pustych/ braków w kolumnach
 x = clean_data[['Year', 'Revenue_EUR']] # Dane wejściowe
 y = clean_data.Units_Sold # Ma przewidzeć sprzedać u.Solds
 
-model = tree.DecisionTreeRegressor() # model drzewa decyzyjnego czy cos takiego bylo
+model = tree.DecisionTreeRegressor() # model drzewa decyzyjnego
 
 model.fit(x, y) #ustawienie fitu
 
 p1 = model.predict(x) #predykcja
-print(p1)
+# print(p1)
 
-p2 = model.predict([[2025, 1000000]]) #predykcja 2
-print(p2)
 
 # Cala predykcja dla Data Frame
 df = pd.DataFrame({
@@ -27,4 +27,37 @@ df = pd.DataFrame({
 })
 
 p3 = model.predict(df)
-print(p3)
+# print(p3)
+
+print('---------------------')
+print('Mean Absolute Error:')
+#Mean absolute error
+mea = mean_absolute_error(y, p1)
+
+print(p1.mean())
+print(mea)
+print((mea / p1.mean() * 100))
+
+r2score = r2_score(y, p1)
+print('r2')
+print(r2score)
+
+print('-------')
+# train_test_split
+print('Train Test Split')
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+model2 = tree.DecisionTreeRegressor()
+model2.fit(x_train, y_train)
+
+p2 = model2.predict(x_test)
+
+mea = mean_absolute_error(y_test, p2)
+
+print(p2.mean())
+print(mea)
+print((mea / p2.mean() *100))
+
+r2score = r2_score(y_test, p2)
+
+print(r2score)
--- scikitTask.py
+++ scikitTask.py
@@ -3,14 +3,14 @@ from sklearn import tree
 from sklearn.metrics import mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 
-data = pd.read_csv('data/melb_data.csv')
+data = pd.read_csv('data/bmw_global_sales_2018_2025.csv')
 
-clean_data = data[['Price', 'Distance', 'Rooms', 'YearBuilt', 'Car', 'Lattitude', 'Longtitude']]
+clean_data = data[['Year', 'Units_Sold', 'Avg_Price_EUR', 'Revenue_EUR', 'GDP_Growth']]
 
 clean_data = clean_data.dropna()
 
-x = clean_data[['Distance', 'Rooms', 'YearBuilt', 'Car', 'Lattitude', 'Longtitude']]
-y = clean_data.Price
+x = clean_data[['Year', 'Avg_Price_EUR', 'GDP_Growth', 'Revenue_EUR']]
+y = clean_data.Units_Sold
 
 model = tree.DecisionTreeRegressor()
 
@@ -30,17 +30,18 @@ print(r2score)
 
 print('---------------------------------------------------------------------------')
 
-#print(train_test_split(x, y))
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 model2 = tree.DecisionTreeRegressor()
 model2.fit(x_train, y_train)
 
 p2 = model2.predict(x_test)
+
 mea = mean_absolute_error(y_test, p2)
+
 print(p2.mean())
 print(mea)
-print((mea / p2.mean() *100))
+print((mea / p2.mean() * 100))
 
 r2score = r2_score(y_test, p2)
 
